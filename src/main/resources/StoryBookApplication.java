6d6d778e49ccbd3b5de811f85d42e8c47b3c958f
Arpan Mondal <arpan2003mondal@gmail.com>
2026-03-26 14:12:30 +0530

    feat: Add StoryBookApplication class with DotenvInitializer and enable async processing

--- src/main/resources/StoryBookApplication.java
+++ src/main/resources/StoryBookApplication.java
@@ -3,8 +3,10 @@ package com.company.storybook;
 import com.company.storybook.config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class StoryBookApplication {
 
 	public static void main(String[] args) {
